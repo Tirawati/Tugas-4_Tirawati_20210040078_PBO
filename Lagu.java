2288825bc45226c5d3c7d84443d7c60410bf3201
Tirawati <114417749+Tirawati@users.noreply.github.com>
2022-10-22 20:33:57 +0700

    Add files via upload

---
+++ Lagu.java
@@ -0,0 +1,12 @@
+public class Lagu {
+    
+    Protected String nama_lagu; 
+    String nama_album;
+    
+    public Lagu(String nama_album) {
+        this.nama_album = nama_album;
+    }
+    public void infoLagu(){
+        System.out.println("Saya suka mendengarkan lagu");
+    }
+}
